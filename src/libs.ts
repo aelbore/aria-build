fd5867d12ef32bd8cb3a4248360dd1343d68e1c0
aelbore <aelbore.sg@gmail.com>
2019-05-01 10:19:19 +0100

    aria builds

---
+++ src/libs.ts
@@ -0,0 +1,7 @@
+
+const commonjs = require('rollup-plugin-commonjs')
+const typescript2 = require('rollup-plugin-typescript2')
+const nodeResolve = require('rollup-plugin-node-resolve')
+
+export { rollup } from 'rollup'
+export { commonjs, nodeResolve, typescript2 }
