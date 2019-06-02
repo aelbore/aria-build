00c88a0b8cc2edeec7f747b19d5cc1dce3b35a93
aelbore <aelbore.sg@gmail.com>
2019-06-02 21:55:36 +0800

    add dependencies

--- src/libs.ts
+++ src/libs.ts
@@ -3,7 +3,8 @@ import minifyHTML from 'rollup-plugin-minify-html-literals'
 const commonjs = require('rollup-plugin-commonjs')
 const typescript2 = require('rollup-plugin-typescript2')
 const nodeResolve = require('rollup-plugin-node-resolve')
+const alias = require('rollup-plugin-strict-alias')
 
 export { rollup } from 'rollup'
 export { terser } from 'rollup-plugin-terser'
-export { commonjs, nodeResolve, typescript2, minifyHTML }
+export { commonjs, nodeResolve, typescript2, minifyHTML, alias }
