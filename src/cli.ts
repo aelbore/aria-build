549729146d04e95e1a715f29654e9f75efd5fe16
aelbore <aelbore.sg@gmail.com>
2020-02-14 10:10:10 +0800

    fix issue on cli.ts file

--- src/cli.ts
+++ src/cli.ts
@@ -5,12 +5,12 @@ export async function run(version: string) {
   const cliOptions = getCliOptions()
   const program = require('sade')(cliOptions.package, true)
 
+  await Promise.all(cliOptions.options.map(option => {
+    program.option(option.alias, option.description, option.defaultValue ?? null)
+  }))
+
   program
     .version(version)
     .action(handler)
     .parse(process.argv)
-
-  await Promise.all(cliOptions.options.map(option => {
-    program.option(option.alias, option.description, option.defaultValue ?? null)
-  }))
 }
