5c0b114a19d11cbdf7a49d66c7417984567bbca6
aelbore <aelbore.sg@gmail.com>
2020-02-14 10:10:10 +0800

    add file cli options, dynamicaly load options from config

--- src/cli.ts
+++ src/cli.ts
@@ -1,24 +1,16 @@
-import { DEFAULT_OUT_DIR } from './cli-common'
 import { handler } from './cli-handler'
+import { getCliOptions } from './cli-options'
 
 export async function run(version: string) {
-  const program = require('sade')('aria-build', true)
+  const cliOptions = getCliOptions()
+  const program = require('sade')(cliOptions.package, true)
 
   program
     .version(version)
-    .option('-d, --declaration', 'Generates corresponding .d.ts file', false)
-    .option('-f, --format', 'build specified formats', 'es,cjs')
-    .option('-i, --entry', 'Entry module(s)')
-    .option('-o, --output', 'Directory to place build files into', DEFAULT_OUT_DIR)
-    .option('-c, --config', 'config file of aria-build. i.e aria.config.ts')
-    .option('-w, --watch', 'Rebuilds on any change  (default false)', false)
-    .option('--external', 'Specify external dependencies')
-    .option('--resolve', 'Resolve dependencies')
-    .option('--clean', 'Clean the dist folder default', DEFAULT_OUT_DIR)
-    .option('--globals', 'Specify globals dependencies')
-    .option('--sourcemap', 'Generate source map')
-    .option('--name', 'Specify name exposed in UMD builds')
-    .option('--compress', 'Compress or minify the output')
     .action(handler)
     .parse(process.argv)
+
+  await Promise.all(cliOptions.options.map(option => {
+    program.option(option.alias, option.description, option.defaultValue ?? null)
+  }))
 }
