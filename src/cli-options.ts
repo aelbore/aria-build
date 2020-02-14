2799d869df0b5758486a5885ee598a5ff2e6acbf
aelbore <aelbore.sg@gmail.com>
2020-02-14 10:10:10 +0800

    add file cli options, dynamicaly load options from config

---
+++ src/cli-options.ts
@@ -0,0 +1,19 @@
+export const getCliOptions = () => ({
+  package: 'aria-build',
+  command: 'build',
+  options: [
+    { alias: '-i, --entry', description: 'Entry modules' },
+    { alias: '-d, --declaration', description: 'Generates corresponding .d.ts file', defaultValue: false },
+    { alias: '-f, --format', description: 'Build specified formats', defaultValue: 'es,cjs' },
+    { alias: '-o, --output', description: 'Directory to place build files into', defaultValue: 'dist' },
+    { alias: '-c, --config', description: 'config file of aria-build. i.e aria.config.ts' },
+    { alias: '-w, --watch', description: 'Rebuilds on any change  (default false)', defaultValue: false },
+    { alias: '--external', description: 'Specify external dependencies' },
+    { alias: '--name', description: 'Specify name exposed in UMD builds' },
+    { alias: '--globals', description: 'Specify global dependencies' },
+    { alias: '--compress', description: 'Compress or minify the output' },
+    { alias: '--sourcemap', description: 'Generate sourcemap' },
+    { alias: '--resolve', description: 'Resolve dependencies' },
+    { alias: '--clean', description: 'Clean the dist folder default (dist)', defaultValue: 'dist' }        
+  ]
+})
