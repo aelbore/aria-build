e29a642728526cc3c24bdea9d84fd207ce6b2cfd
aelbore <aelbore.sg@gmail.com>
2020-02-18 10:10:10 +0800

    add watch

--- src/build.ts
+++ src/build.ts
@@ -4,7 +4,7 @@ import { copyPackageFile, copyReadmeFile, renameDtsEntryFile, moveDtsFiles } fro
 
 export async function rollupGenerate({ inputOptions, outputOptions }) {
 	const bundle = await rollup(inputOptions)
-	const result  = await bundle.generate(outputOptions)
+	const result = await bundle.generate(outputOptions)
 	return result.output
 }
 
