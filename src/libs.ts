87735dd7cd8058e473fb83f524148c0fb5c9220d
aelbore <aelbore.sg@gmail.com>
2020-04-28 15:42:33 +0800

    add rollup plugin and tests

--- src/libs.ts
+++ src/libs.ts
@@ -1,6 +1,8 @@
 import * as url from '@rollup/plugin-url'
+import * as ts from 'typescript'
 import minifyHTML from 'rollup-plugin-minify-html-literals'
 import json from '@rollup/plugin-json'
+import MagicString from 'magic-string'
 
 const commonjs = require('@rollup/plugin-commonjs')
 const nodeResolve = require('@rollup/plugin-node-resolve')
@@ -11,3 +13,4 @@ const replacePlugin = require('@rollup/plugin-replace')
 export { rollup } from 'rollup'
 export { terser } from 'rollup-plugin-terser'
 export { commonjs, replacePlugin, json, url, nodeResolve, typescript2, minifyHTML, multiEntry }
+export { MagicString, ts }
