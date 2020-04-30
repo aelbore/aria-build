614a96e670562690d083de9548a12e47604ab493
aelbore <aelbore.sg@gmail.com>
2020-04-30 16:18:43 +0800

    add hmr options, add tests

--- src/build/hmr-build.ts
+++ src/build/hmr-build.ts
@@ -1,30 +1,33 @@
 import { hmrPlugin, watch as rollupWatch } from '../libs'
-import { CreateRollupConfigOptions, _createTSRollupConfig } from '../config/config'
+import { CreateRollupConfigOptions, _createTSRollupConfig, RollupConfigBase } from '../config/config'
 
 import { rollupBuild } from './build'
 
 export async function hmrBuild(options: CreateRollupConfigOptions) {
+  const config = options.config as RollupConfigBase
+
   const { inputOptions, outputOptions } = _createTSRollupConfig(options)
   await rollupBuild({ inputOptions, outputOptions })
   
   const { input, plugins, watch } = inputOptions
   const { sourcemap, file, format } = outputOptions
 
-  rollupWatch({
-    input,
-    plugins: [
-      ...(plugins as any[]),
-      hmrPlugin({
-        public: 'public',
-        clearConsole: false,
-        write: true
-      })
-    ],
-    watch,
-    output: {
-      sourcemap,
-      file,
-      format
-    }
-  })
+  config.hmr &&
+    rollupWatch({
+      input,
+      plugins: [
+        ...(plugins as any[]),
+        hmrPlugin({
+          public: 'public',
+          clearConsole: false,
+          write: true
+        })
+      ],
+      watch,
+      output: {
+        sourcemap,
+        file,
+        format
+      }
+    })
 }
--- src/build/hrm-build.spec.ts
+++ src/build/hrm-build.spec.ts
@@ -9,35 +9,8 @@ import { hmrBuild } from './hmr-build'
 describe('hrmBuild', () => {
   let libs: typeof import('../libs')
   let config: typeof import('../config/config')
-  
-  before(async () => {
-    [ libs, config ] = await Promise.all([  import('../libs'), import('../config/config') ])
-  })
 
-  afterEach(() => {
-    sinon.restore()
-    mockfs.restore()
-  })
-
-  it('should build and watch with config', async () => {
-    mockfs({
-      'dist': {},
-      './src/input.ts': `import * as fs from 'fs'`
-    })
-
-    const name = 'aria-hmr'
-
-    const configOptions: TSRollupConfig = {
-      input: './src/index.js',
-      external: [ 'fs' ],
-      plugins: [],
-      output: {
-        file: 'public/bundle.js',
-        sourcemap: true,
-        format: 'es'
-      }
-    }
-
+  function createStubs(configOptions: TSRollupConfig) {
     const options = {
       inputOptions: {
         input: './src/input.ts',
@@ -56,10 +29,63 @@ describe('hrmBuild', () => {
     const watchStub = sinon.stub(libs, 'watch').returns(void 0)
     const hmrPluginStub = sinon.stub(libs, 'hmrPlugin').returns(void 0)
 
+    return { createTSRollupConfigStub, watchStub, hmrPluginStub }
+  }
+
+  function createConfigOptions(opts?: TSRollupConfig) {
+    const configOptions: TSRollupConfig = {
+      input: './src/index.js',
+      external: [ 'fs' ],
+      plugins: [],
+      output: {
+        file: 'public/bundle.js',
+        sourcemap: true,
+        format: 'es'
+      },
+      ...(opts ?? {})
+    }
+    return configOptions
+  }
+  
+  before(async () => {
+    [ libs, config ] = await Promise.all([  import('../libs'), import('../config/config') ])
+  })
+
+  beforeEach(() => {
+    mockfs({
+      'dist': {},
+      './src/input.ts': `import * as fs from 'fs'`
+    })
+  })
+
+  afterEach(() => {
+    sinon.restore()
+    mockfs.restore()
+  })
+
+  it('should build and watch with config hmr disabled', async () => {
+    const name = 'aria-hmr'
+
+    const configOptions = createConfigOptions()
+    const { watchStub, createTSRollupConfigStub, hmrPluginStub } = createStubs(configOptions)
+
     await hmrBuild({ config: configOptions, name })
 
-    expect(watchStub.called).toBeTrue()
     expect(createTSRollupConfigStub.called).toBeTrue()
+    expect(watchStub.called).toBeFalse()
+    expect(hmrPluginStub.called).toBeFalse()
+  })
+
+  it('should build and watch with config hmr enable', async () => {
+    const name = 'aria-hmr'
+
+    const configOptions = createConfigOptions({ hmr: true })
+    const { watchStub, createTSRollupConfigStub, hmrPluginStub } = createStubs(configOptions)
+
+    await hmrBuild({ config: configOptions, name })
+
+    expect(createTSRollupConfigStub.called).toBeTrue()
+    expect(watchStub.called).toBeTrue()
     expect(hmrPluginStub.called).toBeTrue()
   })
 
