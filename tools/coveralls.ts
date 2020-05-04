63de33242d45476a273697b35f65483e780f7edd
aelbore <aelbore.sg@gmail.com>
2020-05-04 14:57:19 +0800

    fixed script for coveralls

--- tools/coveralls.ts
+++ tools/coveralls.ts
@@ -1,11 +1,7 @@
 (async function() {
   const { exec } = await import('../dist/aria-build')
 
-  async function execute(command: string) {
-    await exec(`${command} ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js`)
-  }
-
   process.platform.includes('win32') 
-    ? await execute('type')
-    : await execute('cat')
+    ? await exec(`type coverage/lcov.info | coveralls/bin/coveralls.js`)
+    : await exec(`cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js`)
 })()
