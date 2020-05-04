12115829b6460eba4a42a32244a922692dfc8813
aelbore <aelbore.sg@gmail.com>
2020-05-04 15:05:29 +0800

    fixed script for coveralls, run only on linux os

--- tools/coveralls.ts
+++ tools/coveralls.ts
@@ -1,7 +1,6 @@
 (async function() {
   const { exec } = await import('../dist/aria-build')
 
-  process.platform.includes('win32') 
-    ? await exec(`type coverage/lcov.info | coveralls/bin/coveralls.js`)
-    : await exec(`cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js`)
+  process.platform.includes('linux') 
+    && await exec(`cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js`)
 })()
