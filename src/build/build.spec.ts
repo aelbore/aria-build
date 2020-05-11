9f92a5145a74037cb3b900bc1844646b2cbaadba
aelbore <aelbore.sg@gmail.com>
2020-05-11 22:41:37 +0800

    add test, update build to support esbuild

--- src/build/build.spec.ts
+++ src/build/build.spec.ts
@@ -1,11 +1,13 @@
 import * as sinon from 'sinon'
 import * as mockfs from 'mock-fs'
 import * as fs from 'fs'
+import * as mock from 'mock-require'
 
 import { expect } from 'aria-mocha'
 import { TSRollupConfig, ConfigResult } from '../config/config'
 
 import { build, _build, ebuild } from './build'
+import { Service, TransformResult, TransformOptions } from 'esbuild'
 
 describe('build', () => {
   let config: typeof import('../config/config')
@@ -24,6 +26,7 @@ describe('build', () => {
   afterEach(() => {
     mockfs.restore()
     sinon.restore()
+    mock.stopAll()
   })
 
   it('should build with config', async () => {
@@ -121,6 +124,48 @@ describe('build', () => {
     await ebuild({ config: configOptions, name: 'aria-test' })
 
     expect(createTSRollupConfigStub.called).toBeTrue()
+    expect(fs.existsSync('./dist/input.js')).toBeTrue()
+  }) 
+
+  it('should ebuild with config with esbuild', async () => {
+    const options = [
+      {
+        inputOptions: { input: './src/input.ts', external: [ 'fs' ] },
+        outputOptions: { file: './dist/input.js', format: 'es' }
+      }
+    ]
+
+    const createRollupConfigsStub = sinon
+      .stub(config, 'createRollupConfigs')
+      .returns(options as ConfigResult[])
+
+    const configOptions = {
+      input: './src/input.ts'
+    }
+
+    const mockEsbuild = {
+      startService(): Promise<Service> { 
+        function stop(): void { }
+
+        function transform(file: string, 
+          options: TransformOptions): Promise<TransformResult> {
+            const result: TransformResult = {
+              warnings: []
+            }
+            return Promise.resolve(result)
+        }
+
+        const result: Service = { stop, transform }
+
+        return Promise.resolve(result)
+      }
+    }
+
+    mock('esbuild', mockEsbuild)
+    
+    await ebuild({ config: configOptions, name: 'aria-test', esbuild: true })
+
+    expect(createRollupConfigsStub.called).toBeTrue()
     expect(fs.existsSync('./dist/input.js')).toBeTrue()
   }) 
 
