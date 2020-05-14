40ab60c3d0adb8eb3c00ac768d8c98214dd89164
aelbore <aelbore.sg@gmail.com>
2020-05-14 11:22:01 +0800

    structure the code

--- tools/bundle.ts
+++ tools/bundle.ts
@@ -1,9 +1,8 @@
-import { clean, DEFAULT_VALUES, onwarn, copyReadMeFile, mkdir, copyPackageFile, esBuildPlugin } from '../src'
+import { clean, DEFAULT_VALUES, onwarn, terser, copyReadMeFile, mkdir, copyPackageFile, esBuildPlugin, TSRollupConfig, buildConfig } from '../src'
 import { plugins } from './plugins'
+import { esbundle } from '../src/esbuild/esbuild'
 
-import { rollup } from 'rollup'
 import { builtinModules } from 'module'
-import dts from 'rollup-plugin-dts'
 
 (async function() {
   const pkg = require('../package.json')
@@ -16,47 +15,15 @@ import dts from 'rollup-plugin-dts'
     ...DEFAULT_VALUES.ROLLUP_EXTERNALS
   ]
 
-  const createDtsFile = async () => {
-    const opts = {
-      input: './src/index.ts',
-      external,
-      plugins: [ dts() ],
-      onwarn,
-      output: {
-        file: './dist/aria-build.d.ts'
-      }
-    }
-
-    const bundle = await rollup(opts)
-    await bundle.write(opts.output)
-  }
-
-  const build = async () => {
-    const options: any = {
-      input: './src/index.ts',
-      external,
-      plugins: [ 
-        esBuildPlugin(),
-        ...plugins
-      ],
-      onwarn,
-      output: [
-        {
-          file: './dist/aria-build.es.js',
-          format: 'es'
-        },
-        {
-          file: './dist/aria-build.js',
-          format: 'cjs'
-        }
-      ]
-    }
-  
-    const bundle = await rollup(options)
-    await Promise.all(options.output.map(bundle.write))
-  }
+  const config = buildConfig({
+    format: 'es,cjs',
+    dependencies: external,
+    output: 'dist',
+    pkgName: 'aria-build',
+    declaration: true
+  })
 
   await clean('dist')
   await mkdir('dist')
-  await Promise.all([ build(), createDtsFile(), copyReadMeFile(), copyPackageFile() ])
+  await esbundle({ config, name: 'aria-build', esbuild: true  })
 })()
