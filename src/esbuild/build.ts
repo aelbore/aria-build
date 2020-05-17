7c9e6b1fc2491053444b7cd6a9e9fa6c1e344776
aelbore <aelbore.sg@gmail.com>
2020-05-17 16:24:41 +0800

    add test, support beforeAfter plugins

--- src/esbuild/build.ts
+++ src/esbuild/build.ts
@@ -1,34 +1,38 @@
 import { rollup, esBuildPlugin, commonjs } from '../libs'
 import { CreateRollupConfigOptions, TSRollupConfig } from '../config/config'
-import { DEFAULT_VALUES } from '../common/common'
+import { DEFAULT_VALUES, PluginBeforeAfter } from '../common/common'
 
 export async function esbuild(options: CreateRollupConfigOptions) {
   const { config, esbuild } = options
   const opts = Array.isArray(config) ? config: [ config ]
 
   return Promise.all(opts.map(async opt => {
-    const { input, plugins, external, output, commonOpts } = opt as TSRollupConfig
+    const { input, output, commonOpts } = opt as TSRollupConfig
 
     const mutiplyEntryPlugin = () => 
        Array.isArray(input) ? [ require('@rollup/plugin-multi-entry')() ]: []
 
     const esBuildPluginEntry = () => esbuild ? [ esBuildPlugin() ]: []
 
-    const bundle = await rollup({
-      input, 
-      plugins: [
-        ...((plugins ?? []) as any[]),
-        ...esBuildPluginEntry(),
-        ...mutiplyEntryPlugin(),
-        commonjs(commonOpts)
-      ], 
-      external: [
-        ...(external ?? []),
-        ...DEFAULT_VALUES.ROLLUP_EXTERNALS
-      ]
-    })
+    const plugins = [
+      ...(opt.plugins?.hasOwnProperty('before') 
+            ? (opt.plugins as PluginBeforeAfter).before: []),
+      ...(Array.isArray(opt.plugins) ? opt.plugins: []),
+      ...(opt.plugins?.hasOwnProperty('after') 
+            ? (opt.plugins as PluginBeforeAfter).after: []),
+      ...esBuildPluginEntry(),
+      ...mutiplyEntryPlugin(),
+      commonjs(commonOpts)
+    ]
+
+    const external = [
+      ...(opt.external ?? []),
+      ...DEFAULT_VALUES.ROLLUP_EXTERNALS
+    ]
+
     const outputs: any[] = Array.isArray(output) ? output: [ output ]
-    
+
+    const bundle = await rollup({ input, plugins, external })
     return Promise.all(outputs.map(bundle.write))
   }))
 }
