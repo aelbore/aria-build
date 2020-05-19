9cf26ea4186409e38eeb932c3d00923db5aec910
aelbore <aelbore.sg@gmail.com>
2020-05-19 09:04:59 +0800

    remove hrm code and plugin

--- src/libs.ts
+++ src/libs.ts
@@ -11,11 +11,10 @@ const nodeResolve = require('@rollup/plugin-node-resolve')
 const multiEntry = require('@rollup/plugin-multi-entry')
 const typescript2 = require('rollup-plugin-typescript2')
 const replacePlugin = require('@rollup/plugin-replace')
-const hmrPlugin = require('rollup-plugin-hot')
 
 export { rollup, watch, WatcherOptions, ModuleFormat } from 'rollup'
 export { terser } from 'rollup-plugin-terser'
 export { commonjs, replacePlugin, json, url, nodeResolve, typescript2, minifyHTML, multiEntry }
-export { MagicString, ts, hmrPlugin }
+export { MagicString, ts }
 
 export * from './plugins/rollup-plugin-esbuild'
