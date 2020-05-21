cf3af438f1b7c3048b14265930230c97d170a218
aelbore <aelbore.sg@gmail.com>
2020-05-21 13:04:56 +0800

    add tests, refactor codes

--- src/esbuild/build.ts
+++ src/esbuild/build.ts
@@ -1,9 +1,10 @@
-import { rollup, esBuildPlugin, commonjs } from '../libs'
+import { rollup, esBuildPlugin, commonjs, terser } from '../libs'
 import { CreateRollupConfigOptions, DEFAULT_VALUES, PluginOptions, onwarn } from '../common/common'
 import { swcPlugin } from '../plugins/rollup-plugin-swc'
 import { resolvePathPlugin } from '../plugins/rollup-plugin-resolve-path'
 
 type TSRollupConfig = import('../common/common').TSRollupConfig
+type RollupConfigBase = import('../common/common').RollupConfigBase
 type PluginBeforeAfter = import('../common/common').PluginBeforeAfter
 
 function buildPlugins({ swc, esbuild }) {
@@ -29,29 +30,50 @@ function flatPlugins(plugins: PluginOptions) {
   ]
 }
 
+export function createConfig(options: CreateRollupConfigOptions) {
+  const { esbuild, swc } = options
+  const config = options.config as RollupConfigBase
+
+  const { input, output, commonOpts, compress } = config 
+
+  const mutiplyEntryPlugin = () => 
+     Array.isArray(input) ? [ require('@rollup/plugin-multi-entry')() ]: []
+
+  const outputs: any[] = Array.isArray(output) ? output: [ output ]
+
+  const plugins = [
+    ...mutiplyEntryPlugin(),
+    ...flatPlugins(config.plugins),
+    ...buildPlugins({ swc, esbuild }),
+    commonjs(commonOpts),
+    ...(compress ? [ terser() ]: [])
+  ]
+
+  const external = [
+    ...(config.external ?? []),
+    ...DEFAULT_VALUES.ROLLUP_EXTERNALS
+  ]
+
+  const opts: TSRollupConfig = {
+    input,
+    plugins,
+    external,
+    output: outputs
+  }
+
+  return opts
+}
+
 export async function esbuild(options: CreateRollupConfigOptions) {
   const { config, esbuild, swc } = options
   const opts = Array.isArray(config) ? config: [ config ]
 
   return Promise.all(opts.map(async opt => {
-    const { input, output, commonOpts } = opt as TSRollupConfig
+    const config = await createConfig({ swc, esbuild, config: opt })
+    const { input, external } = config
 
-    const mutiplyEntryPlugin = () => 
-       Array.isArray(input) ? [ require('@rollup/plugin-multi-entry')() ]: []
-    
-    const plugins = [
-      ...mutiplyEntryPlugin(),
-      ...flatPlugins(opt.plugins),
-      ...buildPlugins({ swc, esbuild }),
-      commonjs(commonOpts)
-    ]
-
-    const external = [
-      ...(opt.external ?? []),
-      ...DEFAULT_VALUES.ROLLUP_EXTERNALS
-    ]
-
-    const outputs: any[] = Array.isArray(output) ? output: [ output ]
+    const outputs = config.output as any[]
+    const plugins = config.plugins as any[]
 
     const bundle = await rollup({ input, plugins, external, onwarn })
     return Promise.all(outputs.map(bundle.write))
