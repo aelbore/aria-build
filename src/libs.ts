e867e0c4584bc8671a218df149fb0d6671a36fa1
aelbore <aelbore.sg@gmail.com>
2020-05-22 09:09:07 +0800

    upgrade ts, fixed sinon stub, add importHelpers, refactor code to worked with ts

--- src/libs.ts
+++ src/libs.ts
@@ -5,12 +5,18 @@ import * as ts from 'typescript'
 import minifyHTML from 'rollup-plugin-minify-html-literals'
 import json from '@rollup/plugin-json'
 import MagicString from 'magic-string'
+import * as rollup$ from 'rollup'
 
 const commonjs = require('@rollup/plugin-commonjs')
 const nodeResolve = require('@rollup/plugin-node-resolve')
 const replacePlugin = require('@rollup/plugin-replace')
 
-export { rollup, watch, WatcherOptions, ModuleFormat } from 'rollup'
+export type WatcherOptions = import('rollup').WatcherOptions
+export type ModuleFormat = import('rollup').ModuleFormat
+
+export const rollup = rollup$.rollup
+export const watch = rollup$.watch
+
 export { commonjs, replacePlugin, json, url, nodeResolve, minifyHTML }
 export { MagicString, ts }
 
