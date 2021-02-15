c43e6b537dd1c70d751708d198756bb3155c708b
aelbore <aelbore.sg@gmail.com>
2021-02-15 16:54:08 +0800

    rollback ts-node, update dependencies

--- bin/aria-build.js
+++ bin/aria-build.js
@@ -1,12 +1,38 @@
 #!/usr/bin/env node
 
-require('child_process')
-  .spawnSync('ts-esm', [ 
-    '--no-warnings',
-    '--experimental-​modules',
-    '--experimental-json-modules',
-    './bin/exec.mjs',
-    ...process.argv.slice(2) 
-  ], { 
-    stdio: 'inherit' 
-  })
+const path = require('path')
+const fs = require('fs')
+
+const TS_CONFIG_PATH = path.resolve('tsconfig.json')
+
+const tsconfigDefaults = {
+  "target": "es2018",
+  "module": "commonjs",
+  "lib" :[ "dom", "es2015", "es2017" ],
+  "allowJs": true
+}
+
+const compilerOptions = (fs.existsSync(TS_CONFIG_PATH) 
+  ? require(TS_CONFIG_PATH).compilerOptions:
+  {}
+)
+
+delete compilerOptions.exclude
+delete compilerOptions.include
+delete compilerOptions.lib
+
+require('ts-node').register({
+  transpileOnly: true,
+  compilerOptions: {
+    ...compilerOptions,
+    ...tsconfigDefaults,
+    lib: [ 
+      ...tsconfigDefaults.lib, 
+      ...(compilerOptions && compilerOptions.lib 
+            ? compilerOptions.lib: [])
+    ]
+  }
+})
+
+const pkg = require('../package.json')
+require('../src').run(pkg.version)
